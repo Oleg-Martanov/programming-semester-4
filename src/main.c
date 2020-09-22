6b4bbe2136f52a85522725a43e65cdd6e7d1574d
Oleg-Martanov <66037250+Oleg-Martanov@users.noreply.github.com>
2020-09-22 19:24:36 +0300

    сделано задание 3

--- src/main.c
+++ src/main.c
@@ -1,18 +1,42 @@
 #include "forth.h"
 #include "words.h"
-
+#include <string.h>
 #include <stdio.h>
 
 #define MAX_DATA 16384
 #define MAX_STACK 16384
 #define MAX_RETURN 16384
 
-int main(void)
+int main(int argc, char** argv)
 {
+    FILE* input;
     struct forth forth = {0};
     forth_init(&forth, stdin, MAX_DATA, MAX_STACK, MAX_RETURN);
     words_add(&forth);
-    forth_run(&forth);
+    
+    for (int i = 1; i < argc; i++){
+        input = fopen(argv[i],"r");
+                if (input) {
+            forth.input = input;
+           }
+                else if (strcmp(argv[i], "-") == 0) {
+                    forth.input = stdin;
+           }
+                else {
+                 printf("Error: Can not read file %s\n", argv[i]);
+                continue;
+           }
+        forth_run(&forth);
+    }
+    
+    
+    if (argc == 1 ){
+        
+        forth.input = stdin;
+        forth_run(&forth);
+    }
+    
+    
     forth_free(&forth);
     return 0;
 }
